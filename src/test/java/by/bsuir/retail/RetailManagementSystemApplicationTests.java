3a611c373da117887fb5457ad2e44fbff39bf0d3
DEN <mozolden7@gmail.com>
2023-10-19 11:06:35 +0300

    feat: test whether .gitignore configured properly or not

---
+++ src/test/java/by/bsuir/retail/RetailManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bsuir.retail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
